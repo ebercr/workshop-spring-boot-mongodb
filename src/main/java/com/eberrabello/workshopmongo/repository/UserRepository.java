7adb8787122b8c63cddf836317496739737a3cd9
Eber Rabello <ebercr@gmail.com>
2024-06-18 17:24:45 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/eberrabello/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.eberrabello.workshopmongo.repository;
+
+import com.eberrabello.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
