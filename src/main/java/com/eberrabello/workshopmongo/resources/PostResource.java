d9f3b05e7215ddb78e07aea8c0abdc15a025a15a
Eber Rabello <ebercr@gmail.com>
2024-06-19 16:08:37 -0300

    Obtendo um post por id

---
+++ src/main/java/com/eberrabello/workshopmongo/resources/PostResource.java
@@ -0,0 +1,21 @@
+package com.eberrabello.workshopmongo.resources;
+
+import com.eberrabello.workshopmongo.domain.Post;
+import com.eberrabello.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
